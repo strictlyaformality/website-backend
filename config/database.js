57e959cc5b28e58bd4a8be1cf96bd617411b7a50
JONNY!! <jonnykreell@gmail.com>
2020-08-25 10:32:29 -0400

    database config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,26 @@
+let settings = process.env.NODE_ENV === 'production'
+  ? {
+    client: 'postgres',
+    host: env('DATABASE_HOST', '127.0.0.1'),
+    port: env.int('DATABASE_PORT', 27017),
+    database: env('DATABASE_NAME', 'strapi'),
+    username: env('DATABASE_USERNAME', ''),
+    password: env('DATABASE_PASSWORD', ''),
+  }
+  : {
+    client: 'sqlite',
+    filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  };
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
+      settings,
       options: {
         useNullAsDefault: true,
+        ssl: false
       },
     },
   },
